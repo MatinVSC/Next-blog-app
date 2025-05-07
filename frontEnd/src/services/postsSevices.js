d7174524c112207bbe3ea0aea740d65b15694bd0
matin <matinvsc@gmail.com>
2025-05-08 00:14:23 +0330

    handling like and bookmark for blogs

--- frontEnd/src/services/postsSevices.js
+++ frontEnd/src/services/postsSevices.js
@@ -1,5 +1,7 @@
-export async function getPosts() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+import http from "./httpService";
+
+export async function getPosts(queries, options) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list?${queries}`, options);
     const { data } = await response.json()
     const { posts } = data || {};
 
@@ -7,10 +9,18 @@ export async function getPosts() {
 };
 
 
-export async function getPostBySlog(slog) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${slog}`);
+export async function getPostBySlug(slug) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${slug}`);
     const { data } = await response.json();
-    const { post } = data || {};
+    const { posts } = data || {};
 
-    return post;
+    return posts;
+};
+
+export async function likePostApi(postId) {
+    return http.post(`/post/like/${postId}`).then(({ data }) => data.data);
+};
+
+export async function bookmarkPostApi(postId) {
+    return http.post(`/post/bookmark/${postId}`).then(({ data }) => data.data);
 };
