a5f062c87f21a019d33794bb53a58103a31ec43e
matin <matinvsc@gmail.com>
2026-01-15 19:41:04 +0330

    handling create post form page and hooks

---
+++ frontEnd/src/app/(dashboard)/profile/posts/create/_/CreatePostForm.jsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { useCategories } from "@/hooks/useCategories";
+import RHFSelect from "@/ui/RHFSelect";
+import RHFTextField from "@/ui/RHFTextField";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object();
+
+export default function CreatePostForm() {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        reset
+    } = useForm({
+        mode: "onTouched",
+        resolver: yupResolver(schema),
+    });
+
+    const { categories } = useCategories();
+
+    return (
+        <form className="form">
+            <RHFTextField
+                name="title"
+                label="عنوان"
+                errors={errors}
+                register={register}
+                isRequired
+            />
+            <RHFTextField
+                name="briefText"
+                label="متن کوتاه"
+                errors={errors}
+                register={register}
+                isRequired
+            />
+            <RHFTextField
+                name="text"
+                label="متن"
+                errors={errors}
+                register={register}
+                isRequired
+            />
+            <RHFTextField
+                name="slug"
+                label="اسلاگ"
+                errors={errors}
+                register={register}
+                isRequired
+            />
+            <RHFTextField
+                name="readingTime"
+                label="زمان مطالعه"
+                errors={errors}
+                register={register}
+                isRequired
+            />
+            <RHFSelect
+                name="category"
+                label="دسته بندی"
+                errors={errors}
+                register={register}
+                isRequired
+                options={categories}
+            />
+        </form>
+    );
+};
---
+++ frontEnd/src/app/(dashboard)/profile/posts/create/page.jsx
@@ -0,0 +1,21 @@
+import Breadcrumbs from "@/ui/Breadcrumbs";
+import CreatePostForm from "./_/CreatePostForm";
+
+export default function page() {
+    return (
+        <div>
+            <Breadcrumbs breadcrumbs={[
+                {
+                    label: "پست ها",
+                    href: "/profile/posts"
+                },
+                {
+                    label: "ایجاد پست",
+                    href: "/profile/posts/create",
+                    active: true
+                },
+            ]} />
+            <CreatePostForm />
+        </div>
+    );
+};
