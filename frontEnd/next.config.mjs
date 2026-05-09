efa707f66d2aed630347ec3d01e7c020463fdbd4
matin <matinvsc@gmail.com>
2026-05-09 19:59:53 +0330

    feat: Initial setup and configuration for admin panel

--- frontEnd/next.config.mjs
+++ frontEnd/next.config.mjs
@@ -1,22 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true, 
+        
         remotePatterns: [
             {
                 protocol: "http",
                 hostname: "localhost",
                 port: "5000",
-                pathname: "/uploads/**"
-            }
-        ]
+                pathname: "/uploads/**",
+            },
+        ],
     },
     logging: {
         fetches: {
-            fullUrl: true
-        }
-    },
-    experimental: {
-        ppr: "incremental"
+            fullUrl: true,
+        },
     },
 };
 
